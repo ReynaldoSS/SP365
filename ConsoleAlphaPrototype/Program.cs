70dff116fee248d7ad2f7b3ce6d7aa95c6f0a792
Reynaldo Souza Santos <reynaldo.ss@gmail.com>
2021-04-04 23:05:38 -0300

    Criação do projeto ConsoleAlphaProtorype

---
+++ ConsoleAlphaPrototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAlphaPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
